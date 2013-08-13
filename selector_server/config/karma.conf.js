d5c879e3db840dbc855e669f231eabe81ebe1473
Joey Scarr <joey.scarr+github@gmail.com>
2013-08-14 11:41:54 +1200

    Added a testing framework (currently empty)

---
+++ selector_server/config/karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = function(config) {
+	config.set({
+		basePath: '../',
+		
+		frameworks: ['jasmine'],
+		
+		files: [
+			'client/js/lib/*.js',
+			'client/js/*.js',
+			'test/lib/angular-mocks.js',
+			'test/unit/**/*.js'
+		],
+		
+		reporters: ['progress', 'junit'],
+		
+		junitReporter: {
+			// will be resolved to basePath (in the same way as files/exclude patterns)
+			outputFile: 'test_out/unit-test-results.xml'
+		},
+		
+		// enable / disable watching file and executing tests whenever any file changes
+		// CLI --auto-watch --no-auto-watch
+		autoWatch: true,
+		
+		browsers: ['Chrome'],
+		
+		plugins: [
+			'karma-jasmine',
+			'karma-chrome-launcher',
+			'karma-junit-reporter'
+		],
+		
+		// If browser does not capture in given timeout [ms], kill it
+		// CLI --capture-timeout 5000
+		captureTimeout: 20000,
+
+		// Auto run tests on start (when browsers are captured) and exit
+		// CLI --single-run --no-single-run
+		singleRun: false,
+		
+		port: 9876,
+	});
+};
---
+++ selector_server/test/unit/selectionSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* jasmine specs for the selection module go here */
+
+describe('Selection module', function(){
+	beforeEach(module('selection'));
+	
+	it('should ....', inject(function() {
+		//spec body 
+	}));
+
+	it('should ....', inject(function() {
+		//spec body
+	}));
+});
